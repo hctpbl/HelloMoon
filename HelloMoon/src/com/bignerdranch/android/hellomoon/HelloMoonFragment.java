e9c56b2b6bd8bf127aeea5a125265cb87a457816
Héctor Pablos <hctpbl@gmail.com>
2013-08-27 20:19:44 +0200

    Chapter 13 Retained fragment

--- HelloMoon/src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -36,6 +36,12 @@ public class HelloMoonFragment extends Fragment {
 	}
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+	
+	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		mPlayer.stop();
